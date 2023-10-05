5a484f35510762ac0d3c2e896961b4f754a9ac44
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2023-10-05 10:55:25 -0300

    wip: Adicionar Curso Alura

---
+++ alura/course/golang/hello/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nome := "Marcelo"
+	versao := 1.1
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	// Necessário import reflect
+	// fmt.Println("O tipo da variável idade é", reflect.TypeOf(versao))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+	// fmt.Scan(&comando)
+
+	fmt.Println("O endereço da minha variável é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
